5e6bfc21a8aee313ed040f357995b097481018c8
Elizaveta Biryukova <birliza@mail.ru>
2023-09-04 15:44:44 +0200

    fix: change remove

--- src/components/screens/Home/item/Todoitem.jsx
+++ src/components/screens/Home/item/Todoitem.jsx
@@ -5,18 +5,22 @@ import { BsTrash } from 'react-icons/bs'
 
 const Todoitem = ({ todo, changeTodo, removeTodo }) => {
     return (
-        <div className='flex items-center justify-between mb-4 rounded-2xl bg-zinc-800 p-5 w-full'
-            onClick={() => changeTodo(todo._id)}
-        >
-            <button className='flex items-center'>
-                <Check isCompleted={todo.isCompleted} />
-                <span className={cn({
-                    'line-through': todo.isCompleted
-                })}>{todo.title}</span>
-            </button>
+        <div className='flex items-center justify-between mb-4 rounded-2xl bg-zinc-800 p-5 w-full'>
+            <div
+                onClick={() => changeTodo(todo._id)}
+            >
+                <button className='flex items-center'>
+                    <Check isCompleted={todo.isCompleted} />
+                    <span className={cn({
+                        'line-through': todo.isCompleted
+                    })}>{todo.title}</span>
+                </button>
+            </div>
             <button onClick={() => removeTodo(todo._id)}>
+
                 <BsTrash size={22} className='text-gray-600 hover:text-red-700 transition-colors ease-in-out duration-300' />
             </button>
+
         </div>
     )
 }
